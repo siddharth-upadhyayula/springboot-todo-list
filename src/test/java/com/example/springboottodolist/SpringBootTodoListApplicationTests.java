a2b626fb390d699187e4989df10cb31bead54806
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2024-01-11 10:45:20 -0500

    Created the repository

---
+++ src/test/java/com/example/springboottodolist/SpringBootTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
